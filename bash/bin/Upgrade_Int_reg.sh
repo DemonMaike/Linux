4a774c7178c08a0e55809d19830bd80c319e84ff
DemonMaike <dmitry.malyshevv@ya.ru>
2023-05-19 23:25:59 +0400

    Update

--- bash/bin/Upgrade_Int_reg.sh
+++ bash/bin/Upgrade_Int_reg.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-INT=0
+echo -n "Please, input an integer: "
+read INT
 
 if [[ "$INT" =~ ^-?[0-9]+$ ]]; then
 	if [ $INT -eq 0 ]; then
 		echo 'INT is zero.'
 	else
 		if [ $INT -lt 0 ]; then
-			echo 'INT is negative.'
+			echo 'INT is negative'
 		else
-			echo 'INT is positive.'
+			echo 'INT is positive'
 	fi
 
 	if [ $((INT % 2)) -eq 0 ]; then
-		echo 'INT is even.'
+		echo 'INT is even'
 	else
 		echo 'INT is odd'
 	fi
